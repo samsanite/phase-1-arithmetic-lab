ec2da692cc412b9f480cbd09f147fa6886c50dac
Tommy <samsanite25@gmail.com>
2023-04-01 14:24:33 -0700

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+    const num1 = 31;
+    const num2 = 2;
+    const multiply = num1 * num2
+
+    const random = 2;
+    Math.floor(Math.random() * 2) + 1;
+
+    const num3 = 16;
+    const num4 = 6;
+    const mod = num3 % num4;
+    //console.log(mod);
+
+    const max = Math.max(5, 20, 10, 15);
